b6df2238e87833d7cdba7730867ce48a51dc6fce
Rishi Ravikumar <rishiravi.k98@gmail.com>
2023-10-08 19:58:19 -0400

    feat: update joins

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ if __name__ == "__main__":
     conn = getDBConn()
     queryprocessor = queryprocessor.QProcessor()
 
-    query = """SELECT product, product_type FROM routes, products WHERE product = products.product_id AND region_from = 1;"""
-    queryprocessor.processSelectQuery(conn, query)
-    queryprocessor.displayTokens()
+    query1 = """SELECT product, product_type FROM routes, products WHERE product = products.product_id AND region_from = 1;"""
+    query2 = """SELECT region_name from routes, regions, products WHERE product_type = 'ELECTRONICS' AND routes.product = products.product_id AND routes.region_from = regions.region_id;"""
+    queryprocessor.processSelectQuery(conn, query1)
 
     # Creating a cursor object using the cursor() method
     cursor = conn.cursor()
