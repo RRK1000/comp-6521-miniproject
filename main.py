aa29106360ce3682055f5bf258e2204aa2e84983
UJAS BHUVA <ujasbhuva444@gmail.com>
2023-11-17 21:03:58 -0500

    re-formatted main.py file

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def getDBConn():
     conn = psycopg2.connect(
         database="miniproject",
         user="postgres",
-        password="1234",
+        password="password",
         host="127.0.0.1",
         port="5432",
     )
@@ -21,14 +21,12 @@ if __name__ == "__main__":
 
     # query1 = """SELECT product, product_type FROM routes, products WHERE product = products.product_id AND region_from = 1;"""
     # query2 = """SELECT region_name from routes, regions, products WHERE product_type = 'ELECTRONICS' AND routes.product = products.product_id AND routes.region_from = regions.region_id;"""
-    
 
     q1 = "select supplier_name from suppliers, routes where supplier = suppliers.supplier_id AND region_to = 5;"
     q2 = "SELECT product_type, region_name FROM products, regions where origin_region = regions.region_id;"
     q3 = "SELECT product_type FROM products, routes where product = products.product_id AND region_from in (1,2);"
-    # q3 = "SELECT daily_credits, tokens FROM users, generation_history where user = users.id AND users.tokens > 10000;"
-    
-    selectedQuery = q2
+
+    selectedQuery = q3
 
     output = queryprocessor.processSelectQuery(conn, selectedQuery)
     print()
@@ -36,6 +34,9 @@ if __name__ == "__main__":
     for row in output:
         print(row)
     print("Number of rows: ", len(output))
-    print("Time taken by processSelectQuery() in seconds: ", queryprocessor.selectQueryTime)
+    print(
+        "Time taken by processSelectQuery() in seconds: ",
+        queryprocessor.selectQueryTime,
+    )
 
     conn.close()
