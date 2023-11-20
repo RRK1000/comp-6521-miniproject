19bd25e93166e1f28d30b8dae3800fbdac3c4c28
sushant-sinha <sinhasushant04@gmail.com>
2023-11-20 11:07:29 -0400

    added bitmap compression and file check

--- bitmap.py
+++ bitmap.py
@@ -1,6 +1,7 @@
 import psycopg2
 from psycopg2 import sql
 import csv
+import os
 
 # Replace these with your actual database connection parameters
 dbname = 'miniproject'
@@ -10,49 +11,83 @@ host = '127.0.0.1'
 port = '5432'
 
 conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
-cursor = conn.cursor()
 
-bitmap_dict = {}
+def generateBitmap(tableName, attribute, primaryKey_Column, attribute_type):
 
-tableName="routes"
-attribute="route_id"
-primaryKey_Column="route_id"
-# attribute_type can be string or number
-# attribute_type="string"
-attribute_type="number"
+    bitmapAlreadyExists=os.path.isfile('bitmap_index_'+tableName+'_'+attribute+'.csv')
 
-unique_values_query = f'SELECT DISTINCT {attribute} FROM {tableName}'
-cursor.execute(unique_values_query)
-unique_values = [row[0] for row in cursor.fetchall()]
-print(unique_values)
+    if(bitmapAlreadyExists):
+        print("Bitmap already made!")
+        return
+    
+    cursor = conn.cursor()
 
-for value in unique_values:
-    bitmap = 0
-    if attribute_type=="string":
-        rows_with_value_query = f'SELECT {primaryKey_Column} FROM {tableName} WHERE {attribute} = \'{value}\''
-    else:
-        rows_with_value_query = f'SELECT {primaryKey_Column} FROM {tableName} WHERE {attribute} = {value}'
-    cursor.execute(rows_with_value_query)
-    rows_with_value = [row[0] for row in cursor.fetchall()]
+    bitmap_dict = {}
 
-    for row_id in rows_with_value:
-        bitmap |= (1 << row_id)
+    # tableName="routes"
+    # attribute="route_id"
+    # primaryKey_Column="route_id"
+    # # attribute_type can be string or number
+    # # attribute_type="string"
+    # attribute_type="number"
 
-    bitmap_dict[value] = int(bitmap/2)
+    unique_values_query = f'SELECT DISTINCT {attribute} FROM {tableName}'
+    cursor.execute(unique_values_query)
+    unique_values = [row[0] for row in cursor.fetchall()]
 
-csv_file_path = 'bitmap_index_'+tableName+'_'+attribute+'.csv'
+    for value in unique_values:
+        bitmap = 0
+        if attribute_type=="string":
+            rows_with_value_query = f'SELECT {primaryKey_Column} FROM {tableName} WHERE {attribute} = \'{value}\''
+        else:
+            rows_with_value_query = f'SELECT {primaryKey_Column} FROM {tableName} WHERE {attribute} = {value}'
+        cursor.execute(rows_with_value_query)
+        rows_with_value = [row[0] for row in cursor.fetchall()]
 
-with open(csv_file_path, 'w', newline='') as csv_file:
-    csv_writer = csv.writer(csv_file)    
-    csv_writer.writerow(['Value', 'Bitmap'])
+        for row_id in rows_with_value:
+            bitmap |= (1 << row_id)
 
-    for value, bitmap in bitmap_dict.items():
-        Bit=str(bin(bitmap))
-        # to compress bitmap
-        # need to implement compressor
-        compressedBitmap=compressor(Bit[2:][::-1])
-        csv_writer.writerow([value, compressedBitmap])
+        bitmap_dict[value] = int(bitmap/2)
 
-print(f"Bitmap created and saved in are saved in {csv_file_path}")
+    csv_file_path = 'bitmap_index_'+tableName+'_'+attribute+'.csv'
 
-conn.close()
+    with open(csv_file_path, 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)    
+        csv_writer.writerow(['Value', 'Bitmap'])
+
+        for value, bitmap in bitmap_dict.items():
+            Bit=str(bin(bitmap))
+            # to compress bitmap
+            # need to implement compressor
+            compressedBitmap=compressBitmap(Bit[2:][::-1])
+            csv_writer.writerow([value, compressedBitmap])
+
+    print(f"Bitmap created and saved in are saved in {csv_file_path}")
+
+    conn.close()
+
+def compressBitmap(bit):
+
+    compressedBitmap=""
+
+    index=0
+
+    for c in bit:
+        if c=='1':
+
+            runLengthInBinary=bin(index)[2:]
+            runLength=len(runLengthInBinary)
+
+            while runLength!=1:
+                compressedBitmap+='1'
+                runLength-=1
+
+            compressedBitmap+='0'+runLengthInBinary
+
+            print(compressedBitmap)
+            index=0
+        
+        else:
+            index+=1
+
+    return compressedBitmap
